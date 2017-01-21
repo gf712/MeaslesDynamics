60028304df79b75b061ddcca127cc609ca86f69f
Gil <g.f.hoben@gmail.com>
2017-01-21 17:49:52 +0000

    Some more plots and fixed code with birth rates

--- scripts/MeaslesFirstRun.m
+++ scripts/MeaslesFirstRun.m
@@ -1,15 +1,15 @@
 % parameters
-beta = 0.3;
-v = 0.11;
+beta = 2;
+v = 5;
 % maxTime corresponds to 936 weeks/18 years -> 1948 to 1966
-maxTime = 936;
+maxTime = 52;
 %tspan = linspace(0,1,maxTime);
-tspan = [0 maxTime]
+tspan = [0 maxTime];
 
 % UK population
 N = 45000000;
 % Number of infected people at the start
-I0 = 7524;
+I0 = 10000;
 % Number of recovered at the start
 R0 = 0;
 % Number of susceptible at the start (total pop - infected - recovered)
@@ -27,6 +27,7 @@ R=pop(:,3);
 %  Plots the graph
 T=t;
 plot(T,I,'-b');
+title('beta = 2')
 hold on;
 plot(T,R,'-r');
 %plot(T,S,'-k');

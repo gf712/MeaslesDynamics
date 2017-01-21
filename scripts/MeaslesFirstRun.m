764097cbc7d192cd3829ae23308d00c8219dfde8
Gil <g.f.hoben@gmail.com>
2017-01-21 18:00:23 +0000

    fixed some plots of first model

--- scripts/MeaslesFirstRun.m
+++ scripts/MeaslesFirstRun.m
@@ -1,8 +1,8 @@
 % parameters
-beta = 2;
-v = 5;
-% maxTime corresponds to 936 weeks/18 years -> 1948 to 1966
-maxTime = 52;
+beta = 5;
+v = 0.77;
+% maxTime corresponds to 104 weeks -> 2 years
+maxTime = 104;
 %tspan = linspace(0,1,maxTime);
 tspan = [0 maxTime];
 
@@ -11,7 +11,7 @@ N = 45000000;
 % Number of infected people at the start
 I0 = 10000;
 % Number of recovered at the start
-R0 = 0;
+R0 = 2000000;
 % Number of susceptible at the start (total pop - infected - recovered)
 S0 = N-I0-R0;
 S=S0;  
@@ -27,12 +27,12 @@ R=pop(:,3);
 %  Plots the graph
 T=t;
 plot(T,I,'-b');
-title('beta = 2')
+title('beta = 5')
 hold on;
 plot(T,R,'-r');
-%plot(T,S,'-k');
+plot(T,S,'-k');
 ylabel('Number of People');
 xlabel('Time (weeks)');
-legend('Infected','Recovered')
-%legend('Infected','Recovered','Susceptible')
+%legend('Infected','Recovered')
+legend('I','R','S')
 hold off;
